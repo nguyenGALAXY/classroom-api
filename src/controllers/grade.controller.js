7e9f9e35b89d3e8f4edc3f44ede69b6b91ffeb5c
Nguyen Tran <67467375+nguyenGALAXY@users.noreply.github.com>
2021-11-29 20:42:55 +0700

    Arrange grade structure and add column index grade (#21)

--- src/controllers/grade.controller.js
+++ src/controllers/grade.controller.js
@@ -3,26 +3,29 @@ import db from '../models/index'
 import { controller, get, post, del, put } from 'route-decorators'
 import httpStatusCodes from 'http-status-codes'
 import { auth } from '../middleware'
+
+const { Op } = require('sequelize')
+
 import debug from 'src/utils/debug'
+
 
 @controller('/api/classrooms/:id/grades')
 class GradesCtrl extends BaseCtrl {
   @post('/', auth())
   async createGrade(req, res) {
-    let { point, name } = req.body
+    let { point, name, index } = req.body
     let { id: classroomId } = req.params
-
     // TODO: Check user is belong to classroom
 
     if (!name && !point) {
       res.status(httpStatusCodes.BAD_REQUEST).send('Name and point is required')
     }
-
     let grade
     try {
       grade = await db.Grade.create({
         name: name,
         point: point,
+        index: index,
         classroomId,
       })
     } catch (error) {
@@ -30,6 +33,42 @@ class GradesCtrl extends BaseCtrl {
     }
     res.status(httpStatusCodes.OK).send(grade)
   }
+
+  @put('/arrange/:idGrade1/:idGrade2', auth())
+  async arrangeGrade(req, res) {
+    try {
+      const { idGrade1: id1, idGrade2: id2 } = req.params
+      const { id: classroomId } = req.params
+      //Find index 2 grade to swap
+      const indexs = await db.Grade.findAll({
+        attributes: { exclude: ['password'] },
+        where: { [Op.or]: [{ id: id1 }, { id: id2 }], classroomId: classroomId },
+      })
+      //Swap 2 grade index
+      await db.Grade.update(
+        {
+          index: indexs[1].index,
+        },
+        {
+          where: { id: id1 },
+        }
+      )
+      await db.Grade.update(
+        {
+          index: indexs[0].index,
+        },
+        {
+          where: { id: id2 },
+        }
+      )
+      const Grades = await db.Grade.findAll({
+        where: { classroomId },
+      })
+      res.status(httpStatusCodes.OK).json(Grades)
+    } catch (error) {
+      return res.status(500).json({ msg: error.message })
+    }
+
 
   @get('/', auth())
   async getGrades(req, res) {
@@ -81,6 +120,7 @@ class GradesCtrl extends BaseCtrl {
       },
     })
     res.status(httpStatusCodes.OK).send({ message: 'Delete assignment successful' })
+
   }
 }
 
