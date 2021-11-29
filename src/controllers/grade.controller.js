9bfca13cfb6f985ea9eac560f69e9b8299c9bf65
Nguyen Ho <61272412+nguyen315@users.noreply.github.com>
2021-11-29 20:40:39 +0700

    [CLR-017] CRUD assignment (grade) (#19)
    * [CLR-015] Grade API
    
    * [CLR-017] Delete Assignment API

--- src/controllers/grade.controller.js
+++ src/controllers/grade.controller.js
@@ -1,8 +1,9 @@
 import BaseCtrl from './base'
 import db from '../models/index'
-import { controller, get, post, put } from 'route-decorators'
+import { controller, get, post, del, put } from 'route-decorators'
 import httpStatusCodes from 'http-status-codes'
 import { auth } from '../middleware'
+import debug from 'src/utils/debug'
 
 @controller('/api/classrooms/:id/grades')
 class GradesCtrl extends BaseCtrl {
@@ -29,11 +30,29 @@ class GradesCtrl extends BaseCtrl {
     }
     res.status(httpStatusCodes.OK).send(grade)
   }
+
+  @get('/', auth())
+  async getGrades(req, res) {
+    const { id: classroomId } = req.params
+
+    let grades
+
+    try {
+      grades = await db.Grade.findAll({
+        where: {
+          classroomId,
+        },
+      })
+    } catch (error) {
+      debug.log('grade-ctrl', error)
+    }
+    res.status(httpStatusCodes.OK).send(grades)
+  }
+
   @put('/:gradeId', auth())
   async updateGrade(req, res) {
     let { gradeId } = req.params
     let { name, point } = req.body
-    let grade
     if (!name && !point) {
       res.status(httpStatusCodes.BAD_REQUEST).send('Name and point is required')
     }
@@ -47,11 +66,22 @@ class GradesCtrl extends BaseCtrl {
           where: { id: gradeId },
         }
       )
-      await grade.save()
     } catch (error) {
-      console.log(error)
+      debug.log('grade-ctrl', error)
     }
-    res.status(httpStatusCodes.OK).send(grade)
+    res.status(httpStatusCodes.OK).send({ message: 'Update assignment successful' })
+  }
+
+  @del('/:gradeId', auth())
+  async deleteGrade(req, res) {
+    const { gradeId } = req.params
+    await db.Grade.destroy({
+      where: {
+        id: gradeId,
+      },
+    })
+    res.status(httpStatusCodes.OK).send({ message: 'Delete assignment successful' })
   }
 }
+
 export default GradesCtrl
