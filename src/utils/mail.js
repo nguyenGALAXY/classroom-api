d858f35afe8ca52b155b93c80c5245f2b3ede28c
nguyenGALAXY <67467375+nguyenGALAXY@users.noreply.github.com>
2022-01-16 11:04:26 +0700

    Add forgot password and reset password

--- src/utils/mail.js
+++ src/utils/mail.js
@@ -30,6 +30,22 @@ export const generateVerifyEmailTemplate = (url) => {
       </div>
   `
 }
+export const generateResetPasswordEmailTemplate = (url) => {
+  return `
+      <div style="max-width: 700px; margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
+      <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to Classroom</h2>
+      <p>Congratulations! You're almost done.
+          Just click the button below to reset your password.
+      </p>
+      
+      <a href=${url} style="background: crimson; text-decoration: none; color: white; padding: 10px 20px; margin: 10px 0; display: inline-block;">Reset Password</a>
+  
+      <p>If the button doesn't work for any reason, you can also click on the link below:</p>
+  
+      <div>${url}</div>
+      </div>
+  `
+}
 
 export const generateInviteTemplate = (url, role, classroom) => {
   return `
